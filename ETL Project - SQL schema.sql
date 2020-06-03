606c4b25b527dfd8defd82fd67fcdf0aa29d8061
Leishla Perez <leishla.perez42@gmail.com>
2020-06-02 19:12:35 -0500

    Created final notebook and uploaded ERD diagram and SQL schema

---
+++ ETL Project - SQL schema.sql
@@ -0,0 +1,28 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/ijGHGj
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "world_happiness" (
+    "country" str   NOT NULL,
+    "happiness_rank" int   NOT NULL,
+    "happiness_score" int   NOT NULL,
+    "economy_gdp-per-capita" int   NOT NULL,
+    CONSTRAINT "pk_world_happiness" PRIMARY KEY (
+        "country"
+     )
+);
+
+CREATE TABLE "world_bank" (
+    "country" str   NOT NULL,
+    "current_health_expenditure" int   NOT NULL,
+    "domestic_government_health_expenditure" int   NOT NULL,
+    "out-of-pocket_expenditure" int   NOT NULL,
+    CONSTRAINT "pk_world_bank" PRIMARY KEY (
+        "country"
+     )
+);
+
+ALTER TABLE "world_bank" ADD CONSTRAINT "fk_world_bank_country" FOREIGN KEY("country")
+REFERENCES "world_happiness" ("country");
+
